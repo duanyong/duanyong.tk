a5fb93bd9d3c4d4fcb364a8c2471cd451eb8001c
duanyong <coderduan@gmail.com>
2011-02-26 14:56:44 +0800

    action --- 简化a_action_done()，smarty需要渲染的tpl直接从用户请求['QUEST_URL']中分析得到

--- devinc.action.php
+++ devinc.action.php
@@ -10,14 +10,19 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 
-function a_action_done($tpl) {
-    global $arg;
 
-    if (a_bad_string($tpl)
-	|| a_bad_array($arg)
-    ) {
-	a_log();
+function a_action_done() {
+    global $arg;
+    global $_SERVER;
+
+    if (a_bad_string($_SERVER["REQUEST_URI"], $tpl)) {
+	return a_log();
     }
+    
+    // 从用户的请求URL中得到渲染那个tpl文件
+    $tpl = str_replace(".php", "", $tpl);
+    $tpl .= ".tpl";
+
 
     // 取得tpl的绝对路径
     if (strpos($tpl, ROOT_DIR) !== 0) {
@@ -29,6 +34,7 @@ function a_action_done($tpl) {
 
     if (a_bad_file($tpl)) {
 	// 文件不可读
+
 	return a_log();
     }
 
